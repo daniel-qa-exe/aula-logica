bee552af987431ba22673b04d6b2c136962a232f
daniel <daniel.xdkk@gmail.com>
2024-06-13 20:50:16 -0300

    first commit

---
+++ aula_1.py
@@ -0,0 +1,6 @@
+nome = "matheus"
+n1 = 2
+n2 = 3
+nf = n1+n2
+
+print("o aluno ",nome,"teve a primeira nota ",n1,"e a segunda nota ",n2,"e a nopta final ",nf,".")
---
+++ playground.py
@@ -0,0 +1,7 @@
+nome = "PAULA MARTINS"
+mes = "JANEIRO"
+valor = 500,00
+desconto = "10%"
+cupom = "PAULAÉ10"
+
+print("Olá, ", nome,". Em ", mes,"você realizou uma compra no valor de ","R$",valor," e ganhou um desconto de ",desconto," em sua próxima compra. Use o cupom ",cupom,".")
