30397ebec117823b29fe289c0db797dd42bad569
daniel <daniel.xdkk@gmail.com>
2024-06-13 21:32:14 -0300

    add arquivo input

---
+++ input.py
@@ -0,0 +1,8 @@
+#funcao input com algumas a mais ja aprendidas
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade "))
+
+print(type(idade))
+idade_futura = idade + 3 
+
+print(f"O nome da pessoa é {nome}, e a idade {idade},  e a idade futura é {idade_futura}")
